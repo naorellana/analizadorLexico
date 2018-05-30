e0e6bef793739d17375e1b37c1074fe37099e6a2
naorellana <norellanac@miumg.edu.gt>
2018-05-30 15:53:25 -0700

    proyectoconfigurado conspring boot y mvc, stlos con CDN de bootstrap -pruebas con analizadores lexicos

---
+++ src/main/java/com/norllanac/compiladores2018/Models/Test/MainPruebas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.norllanac.compiladores2018.Models.Test;
+
+import java.io.File;
+
+/**
+ *
+ * @author ORELLANA CUY
+ */
+public class MainPruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //carga archivo dentro del proyeccto con la  gramatica,  expresiones
+        //reegulares y reglas que se necessiten en eel anlizador    
+        jflex.Main.generate(new File ("src\\main\\java\\com\\norllanac\\compiladores2018\\Models\\lexer.flex"));
+        
+        
+    }
+}
